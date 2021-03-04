b14b6566888b5b7609eb657ea66402ba9c8b8337
sudhakarsn15 <35915646+sudhakarsn15@users.noreply.github.com>
2021-03-04 17:51:09 +0530

    Map clicking event handler By clicking on marker will be pointer to current location

--- src/App.js
+++ src/App.js
@@ -1,9 +1,55 @@
+import React, { useState } from "react";
 import "./styles.css";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMapEvents
+} from "react-leaflet";
 
+function LocationMarker() {
+  const [position, setPosition] = useState(null);
+  const map = useMapEvents({
+    click() {
+      map.locate();
+    },
+    locationfound(e) {
+      setPosition(e.latlng);
+      map.flyTo(e.latlng, map.getZoom());
+    }
+  });
+  return position === null ? null : (
+    <Marker position={position}>
+      <Popup>You are here</Popup>
+    </Marker>
+  );
+}
 export default function App() {
+  const defaultPosition: LatLngExpression = [48.864716, 2.349];
   return (
     <div className="App">
       <h1>React leaflet tutorial example</h1>
+      <div className="map__container">
+        <MapContainer
+          id="mapid"
+          center={[51.505, -0.09]}
+          zoom={13}
+          scrollWheelZoom={false}
+          style={{ height: "300px", width: "100%" }}
+        >
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[51.505, -0.09]}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+          <LocationMarker />
+        </MapContainer>
+      </div>
     </div>
   );
 }
