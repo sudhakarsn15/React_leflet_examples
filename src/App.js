8c3ad7d2108b49182cbc11aa285873b9e9d5c731
sudhakarsn15 <35915646+sudhakarsn15@users.noreply.github.com>
2021-04-19 16:31:26 +0530

    completed upto events example

--- src/App.js
+++ src/App.js
@@ -28,8 +28,11 @@ function LocationMarker() {
 export default function App() {
   const defaultPosition: LatLngExpression = [48.864716, 2.349];
   return (
-    <div className="App">
+    <div className="App" style={{ textAlign: "left" }}>
       <h1>React leaflet tutorial example</h1>
+      <SetupExample />
+      <PopupWithMarker />
+      <EventsExample />
       <div className="map__container">
         <MapContainer
           id="mapid"
@@ -51,5 +54,61 @@ export default function App() {
         </MapContainer>
       </div>
     </div>
+  );
+}
+
+function SetupExample() {
+  return (
+    <>
+      <h1>Setup</h1>
+      <div>
+        <MapContainer
+          id="mapid"
+          center={[51.505, -0.09]}
+          zoom={13}
+          scrollWheelZoom={false}
+          style={{ height: "300px", width: "100%" }}
+        >
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[51.505, -0.09]}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
+      </div>
+    </>
+  );
+}
+
+function PopupWithMarker() {
+  const position = [51.505, -0.09];
+  return (
+    <>
+      <h1>Popup with Marker</h1>
+      <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </>
+  );
+}
+
+function EventsExample() {
+  return (
+    <>
+      <h1>Events Example</h1>
+      <h3>Click the map to show a marker at your detected location</h3>
+    </>
   );
 }
